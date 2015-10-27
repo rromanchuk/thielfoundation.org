1bbda439759aa1372ccb753771bc20ef6c4f7c94
Ryan Romanchuk <rromanchuk@gmail.com>
2015-10-27 11:09:01 -0700

    Minor css tweaks

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ slimrb -p index.slim > dist/index.html
 slimrb -p peter.slim > dist/peter.html
 slimrb -p jimmy.slim > dist/jimmy.html
 slimrb -p blake.slim > dist/blake.html
+slimrb -p grace.slim > dist/grace.html
 slimrb -p team.slim > dist/team.html
 sass  app.scss > dist/css/app.css
 s3_website push
