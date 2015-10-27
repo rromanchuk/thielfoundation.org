b56a38685417b9eb6fa1467b144af70c8b73e751
Ryan Romanchuk <rromanchuk@gmail.com>
2015-10-27 10:55:11 -0700

    Add content pages

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 slimrb -p index.slim > dist/index.html
+slimrb -p peter.slim > dist/peter.html
+slimrb -p jimmy.slim > dist/jimmy.html
+slimrb -p blake.slim > dist/blake.html
+slimrb -p team.slim > dist/team.html
 sass  app.scss > dist/css/app.css
 s3_website push
