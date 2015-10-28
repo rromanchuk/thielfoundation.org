11099fc7c82d43cb43f37ce3ac82905633ec2b7b
Ryan Romanchuk <rromanchuk@gmail.com>
2015-10-28 16:38:01 -0700

    file extensions are soooo early 2000s

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 slimrb -p app/index.slim > dist/index.html
-slimrb -p app/peter.slim > dist/peter.html
-slimrb -p app/jimmy.slim > dist/jimmy.html
-slimrb -p app/blake.slim > dist/blake.html
-slimrb -p app/grace.slim > dist/grace.html
-slimrb -p app/team.slim > dist/team.html
+slimrb -p app/peter.slim > dist/peter
+slimrb -p app/jimmy.slim > dist/jimmy
+slimrb -p app/blake.slim > dist/blake
+slimrb -p app/grace.slim > dist/grace
+slimrb -p app/team.slim >  dist/team
 sass  app/app.scss > dist/css/app.css
 s3_website push
