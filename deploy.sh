361c6bc5a19cd40b9a9b90f1dfe0429e0d54a994
Ryan Romanchuk <rromanchuk@gmail.com>
2015-10-31 16:22:01 -0700

    Add GA and robots file

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ slimrb -p app/jimmy.slim > dist/jimmy
 slimrb -p app/blake.slim > dist/blake
 slimrb -p app/grace.slim > dist/grace
 slimrb -p app/team.slim >  dist/team
+cp app/robots.txt dist/
 sass  app/app.scss > dist/css/app.css
 s3_website push
