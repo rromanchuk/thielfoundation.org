e9de1ad763abc74e7d7130490086a81a4942d170
Blake Masters <bgmasters@gmail.com>
2015-12-14 15:46:46 -0800

    Add lindy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ slimrb -p app/peter.slim > dist/peter
 slimrb -p app/jimmy.slim > dist/jimmy
 slimrb -p app/blake.slim > dist/blake
 slimrb -p app/grace.slim > dist/grace
+slimrb -p app/lindy.slim >  dist/lindy
 slimrb -p app/team.slim >  dist/team
 cp app/robots.txt dist/
 sass  app/app.scss > dist/css/app.css
